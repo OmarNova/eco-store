beefda0458a83b6e59509f59f781bb3577de3a44
Omar Nova <omar.nova.2020@upb.edu.co>
2022-11-02 14:23:20 -0500

    Update IProduct.ts

--- backend/src/db/mongo/interface/IProduct.ts
+++ backend/src/db/mongo/interface/IProduct.ts
@@ -1,10 +1,6 @@
 import { Document } from "mongoose";
 export default interface IProduct extends Document {
-<<<<<<< Updated upstream
-    id:number;
-=======
     id: number;
->>>>>>> Stashed changes
     nombre: string;
     contenido: string;
     precio: number;
