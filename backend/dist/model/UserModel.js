a7c16dfb4da039fcb5f37a31bc61144127cd77c0
Omar Nova <omar.nova.2020@upb.edu.co>
2022-11-04 11:06:45 -0500

    model

--- backend/dist/model/UserModel.js
+++ backend/dist/model/UserModel.js
@@ -19,17 +19,6 @@ class MoviesModel {
         this.limit = (start, step) => {
             return this.mysqlDBC.limit(start, step);
         };
-        this.getMovies = (limit, fn) => {
-            this.mysqlDBC.connection();
-            const statement = this.mysqlDBC.statement(`SELECT DISTINCT movie.movie_id, title, homepage, overview, popularity, release_date, vote_average, genre_name
-        FROM ??, ??, ?? 
-        WHERE movie.movie_id = movie_genres.movie_id
-        AND movie_genres.genre_id = genre.genre_id
-        ORDER BY title ASC LIMIT ${limit[0]},${limit[1]}`, ['movie', 'genre', 'movie_genres']);
-            this.mysqlDBC.pool.query(statement, (error, rows) => {
-                fn(error, rows);
-            });
-        };
         this.getUser = (email, fn) => {
             this.mysqlDBC.connection();
             const statement = `SELECT * FROM users WHERE email='${email}'`;
@@ -74,24 +63,6 @@ class MoviesModel {
                 fn(error, verified);
             });
         });
-        this.getMovie = (id, fn) => {
-            this.mysqlDBC.connection();
-            const statement = this.mysqlDBC.statement(`SELECT DISTINCT movie.movie_id, title, homepage, overview, popularity, release_date, vote_average, genre_name
-        FROM ??, ??, ?? 
-        WHERE movie.movie_id = movie_genres.movie_id
-        AND movie_genres.genre_id = genre.genre_id
-        AND movie.movie_id = ${id}`, ['movie', 'genre', 'movie_genres']);
-            this.mysqlDBC.pool.query(statement, (error, rows) => {
-                fn(error, rows);
-            });
-        };
-        this.updateTitleMovie = (data, fn) => {
-            this.mysqlDBC.connection();
-            const statement = this.mysqlDBC.statement(`UPDATE movie SET movie.title = ? WHERE movie.movie_id = ?`, [data.title, data.id.toString()]);
-            this.mysqlDBC.pool.query(statement, (error, rows) => {
-                fn(error, rows);
-            });
-        };
         this.mysqlDBC = new MysqlDBC_1.default();
     }
 }
--- backend/src/model/UserModel.ts
+++ backend/src/model/UserModel.ts
@@ -14,18 +14,6 @@ export default class MoviesModel {
         return this.mysqlDBC.limit(start, step);
     };
 
-    public getMovies = (limit: number[], fn: Function) => {
-        this.mysqlDBC.connection();
-        const statement = this.mysqlDBC.statement(`SELECT DISTINCT movie.movie_id, title, homepage, overview, popularity, release_date, vote_average, genre_name
-        FROM ??, ??, ?? 
-        WHERE movie.movie_id = movie_genres.movie_id
-        AND movie_genres.genre_id = genre.genre_id
-        ORDER BY title ASC LIMIT ${limit[0]},${limit[1]}`, 
-        ['movie', 'genre', 'movie_genres']);
-        this.mysqlDBC.pool.query(statement, (error: any, rows: any) => {            
-            fn(error, rows);
-        });              
-    }
 
     public getUser = (email: string, fn: Function) => {
         this.mysqlDBC.connection();
@@ -81,26 +69,4 @@ export default class MoviesModel {
         }); 
     }
 
-
-    public getMovie = (id: number, fn: Function) => {
-        this.mysqlDBC.connection();
-        const statement = this.mysqlDBC.statement(`SELECT DISTINCT movie.movie_id, title, homepage, overview, popularity, release_date, vote_average, genre_name
-        FROM ??, ??, ?? 
-        WHERE movie.movie_id = movie_genres.movie_id
-        AND movie_genres.genre_id = genre.genre_id
-        AND movie.movie_id = ${id}`, 
-        ['movie', 'genre', 'movie_genres']);
-        this.mysqlDBC.pool.query(statement, (error: any, rows: any) => {            
-            fn(error, rows);
-        });
-    }
-
-    public updateTitleMovie = (data: { id: number, title: string }, fn: Function) => {
-        this.mysqlDBC.connection();
-        const statement = this.mysqlDBC.statement(`UPDATE movie SET movie.title = ? WHERE movie.movie_id = ?`, 
-        [data.title, data.id.toString()]);
-        this.mysqlDBC.pool.query(statement, (error: any, rows: any) => {            
-            fn(error, rows);
-        });
-    }
 }
