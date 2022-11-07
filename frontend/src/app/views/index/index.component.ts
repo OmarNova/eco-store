b935e388787a51e3ffdd5832816726746b276af5
Omar Nova <omar.nova.2020@upb.edu.co>
2022-11-07 16:02:19 -0500

    Update index.component.ts

--- frontend/src/app/views/index/index.component.ts
+++ frontend/src/app/views/index/index.component.ts
@@ -124,19 +124,6 @@ export class IndexComponent implements OnInit {
 
   fillButtonCesta (id: string): HTMLDivElement {
 
-    /* 
-
-     <div class="row">
-                            <div class="col" style="text-align: center;">
-                                <button class="btn btn-primary boton" id="btn-modal" type="button">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bag-plus" style="font-size: 15px;">
-                                        <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"></path>
-                                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"></path>
-                                    </svg>&nbsp;Añadir a la cesta</button></div>
-                        </div>
-    
-    */
-
     let row =  document.createElement("div");
     row.classList.add("row");
     row.setAttribute("id",`btn-${id}`)
@@ -156,6 +143,11 @@ export class IndexComponent implements OnInit {
     boton.classList.add("justify-content-center");
     boton.setAttribute("type","button");
 
+    boton.addEventListener("click",event=>{
+      event.preventDefault();
+      this.saveProduct(id);
+    })
+
     let icon = document.createElement("svg");
     icon.classList.add("bi");
     icon.classList.add("bi-bag-plus");
@@ -169,7 +161,6 @@ export class IndexComponent implements OnInit {
     let p = document.createElement("p");
     p.innerHTML = "Añadir a la cesta"
 
-    //boton.innerHTML = "Añadir a la cesta";
     boton.appendChild(icon);
     boton.appendChild(p);
 
@@ -761,7 +752,11 @@ export class IndexComponent implements OnInit {
     
   }
 
-  saveProduct(data: any){
-    localStorage.setItem('id',data(this.dataProducts))
+  saveProduct(id: string){
+    this.api.getProductById(id).subscribe((data:any)=>{
+      console.log(data);
+      //localStorage.setItem('id',data)
+    });
+    //localStorage.setItem('id',data(this.dataProducts))
   }
 }
